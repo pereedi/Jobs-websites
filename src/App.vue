37eec807e096b75826de44da64f5fa5b71321d3c
pereedi <114001685+pereedi@users.noreply.github.com>
2025-02-06 20:45:37 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Hiro from './components/Hiro.vue';
+import Homecards from './components/Homecards.vue';
+import jobListings from './components/JobListings.vue';
+
+</script>
+
+<template>
+  <Navbar/>
+  <Hiro title="Test Title" subtitle="Test Subtitle"/>
+  <Homecards/>
+  <jobListings/>
+  
+</template>
+
